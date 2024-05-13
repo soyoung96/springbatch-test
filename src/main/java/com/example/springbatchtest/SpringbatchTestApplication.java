426a19ab5d6f9c2dde4b7beafbbe5cbb6ba2e2bb
박소영 <psy411301@hyundai-autoever.com>
2024-05-13 11:18:16 +0900

    test

---
+++ src/main/java/com/example/springbatchtest/SpringbatchTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchTestApplication.class, args);
+	}
+
+}
